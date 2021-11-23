94922954ffebef9079af1ae82c8c6971f7a71b2a
chituenguyen <tuenguyen553@gmail.com>
2021-11-23 12:18:47 +0700

    a

--- backend/settings.py
+++ backend/settings.py
@@ -177,16 +177,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 MEDIA_URL = "/images/"
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-    BASE_DIR / 'front-end/build/static'
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'front-end/build/static'),
+)
 
 
 MEDIA_ROOT = BASE_DIR / "static/images"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
